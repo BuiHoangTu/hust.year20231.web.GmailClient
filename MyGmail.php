2b94c7c856adbf17295efaac79db5bc832b7d44d
bhtu <bhtu.work@gmail.com>
2023-12-21 16:26:16 +0700

    sent multiple attachments

--- MyGmail.php
+++ MyGmail.php
@@ -1,5 +1,6 @@
 <?php
 use Google\Service\Gmail;
+
 // Replace With below if future error 
 // use Google_Service_Gmail;
 
@@ -9,7 +10,7 @@ class MyGmail
     public function __construct($client)
     {
         $this->service = new Gmail($client);
-        require_once __DIR__ ."/MessagePage.php";
+        require_once __DIR__ . "/MessagePage.php";
     }
 
     public function getLabels()
@@ -71,7 +72,8 @@ class MyGmail
         return $message;
     }
 
-    public function decodeMessage($message) {
+    public function decodeMessage($message)
+    {
         $service = $this->service;
 
         $messageInParts = $message->getPayload()->getParts();
@@ -87,23 +89,60 @@ class MyGmail
         return base64_decode($out);
     }
 
-    public function sendMessage($to, $subject, $message) {
+    public function sendMessage($to, $subject, $message, $attachments)
+    {
         $service = $this->service;
         $from = $service->users->getProfile("me")->getEmailAddress();
+        $haveAttachments = $attachments != NULL && count($attachments) > 0;
 
         $raw = "From: myAddress<$from>\r\n";
         $raw .= "To: toAddress<$to>\r\n";
         $raw .= "Subject: =?utf-8?B?" . base64_encode($subject) . "?=\r\n";
         $raw .= "MIME-Version: 1.0\r\n";
+
+
+        if ($haveAttachments) {
+            $boundary = uniqid(rand(), true);
+            $raw .= 'Content-type: Multipart/Mixed; boundary="' . $boundary . '"' . "\r\n";
+            // message part 
+            $raw .= "\r\n--{$boundary}\r\n";
+        }
+
+        // with or without attachments
         $raw .= "Content-Type: text/html; charset=utf-8\r\n";
         $raw .= 'Content-Transfer-Encoding: quoted-printable' . "\r\n";
         $raw .= "\r\n$message\r\n";
 
+
+
+        if ($haveAttachments) {
+            // wrap message part if have attachments 
+            $raw .= "--{$boundary}\r\n";
+
+            // attachments
+            for ($i = 0; $i < count($attachments['name']); $i++) {
+                $mimeType = $attachments['type'][$i];
+                $fileName = $attachments['name'][$i];
+                $filesize = $attachments['size'][$i];
+                $filePath = $attachments['tmp_name'][$i];
+
+                $raw .= "\r\n--{$boundary}\r\n";
+                $raw .= 'Content-Type: ' . $mimeType . '; name="' . $fileName . '";' . "\r\n";
+                $raw .= 'Content-ID: <' . $from . '>' . "\r\n";
+                $raw .= 'Content-Description: ' . $fileName . ';' . "\r\n";
+                $raw .= 'Content-Disposition: attachment; filename="' . $fileName . '"; size=' . $filesize . ';' . "\r\n";
+                $raw .= 'Content-Transfer-Encoding: base64' . "\r\n\r\n";
+                $raw .= chunk_split(base64_encode(file_get_contents($filePath)), 76, "\n") . "\r\n";
+                $raw .= "--{$boundary}\r\n";
+            }
+        }
+
+        // encode 
         $encodedRaw = rtrim(strtr(base64_encode($raw), '+/', '-_'), '=');
         $msg = new Gmail\Message();
         $msg->setRaw($encodedRaw);
 
-
+        // send 
         $this->service->users_messages->send("me", $msg);
     }
 }
--- create_mail.php
+++ create_mail.php
@@ -11,26 +11,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $to = $_POST["to"];
     $subject = $_POST["subject"];
     $body = $_POST["body"];
+    $attachments = $_FILES['attachments'];
+
 
     // Send the email
-    $gmail->sendMessage($to, $subject, $body); // Adjust this based on your implementation
+    $gmail->sendMessage($to, $subject, $body, $attachments); // Adjust this based on your implementation
 
     // Optionally, redirect the user to the index page after sending the email
     header("Location: index.php");
     exit(0);
 }
-
-// Display your HTML form for creating a new mail
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <title>Create New Mail</title>
+    <style>
+        .attachment-container {
+            margin-top: 10px;
+        }
+    </style>
+    <script>
+        function addAttachmentInput() {
+            var container = document.getElementById('attachment-container');
+            var newInput = document.createElement('input');
+            newInput.type = 'file';
+            newInput.name = 'attachments[]';
+            container.appendChild(newInput);
+        }
+    </script>
 </head>
 <body>
     <h1>Create New Mail</h1>
 
-    <form method="post" action="">
+    <form method="post" action="" enctype="multipart/form-data">
         <!-- Your form fields go here -->
         <label for="to">To:</label>
         <input type="text" id="to" name="to" required>
@@ -42,6 +58,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         <label for="body">Body:</label>
         <textarea id="body" name="body" required></textarea>
+        <br>
+
+        <!-- File input for initial attachment -->
+        <label for="attachments">Attachments:</label>
+        <input type="file" id="attachments" name="attachments[]" multiple>
+        <br>
+
+        <!-- Container for dynamically added attachments -->
+        <div id="attachment-container" class="attachment-container"></div>
+        
+        <!-- Button to add more attachments -->
+        <button type="button" onclick="addAttachmentInput()">Add Attachment</button>
         <br>
 
         <button type="submit">Send Mail</button>
